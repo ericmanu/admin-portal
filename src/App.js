ebd2183272437686ea2929371af78a909728d51d
ericmanu <eric.manu@amalitech.com>
2023-07-20 14:34:36 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Nav from './Nav';
+import UserInput from './UserInput';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <UserInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.scss';
+
+function Nav() {
+    return (
+        <div>
+            ADMIN PORTAL
+        </div>
+    );
+}
+
+export default Nav;
